caa86c7eec11bbb1ddc70941e207a5b5c5ce9923
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-22 00:14:32 +0545

    make entry employee detail through rails console

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,4 @@
 class Employee < ApplicationRecord
+    validates :first_name, :last_name, :city, :address_line_1, :country, presence: true
+    validates :personal_email, Presence: true, uniqueness: true 
 end
